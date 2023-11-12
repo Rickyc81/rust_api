541a84ad652b18ee4091eeab52913a0d96e53b04
Rickyc81 <riccardo.cosenza81@gmail.com>
2023-11-12 19:18:49 +0100

    commenti e attributo di compilazione dead_code, non_snake_case

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+
 use reqwest;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 
+//Oggetto Todo
 struct Todo {
     userId: u32,
     id: u32,
@@ -12,13 +16,15 @@ struct Todo {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://jsonplaceholder.typicode.com/todos/99";
-    
+    // Url API
+    let url: &str = "https://jsonplaceholder.typicode.com/todos/99";
     // Eseguire la richiesta GET
-    let response = reqwest::get(url).await?;
+    let response: reqwest::Response = reqwest::get(url).await?;
+    // Preleviamo il body in json dalla response
     let json_result: Todo = response.json().await?;
+    // Print della risposta
     println!("My result is: {}, and is marked as: {}", json_result.title, json_result.completed);
-
+    // Restituiamo OK per terminare l'esecuzione con successo
     Ok(())
 }
 
